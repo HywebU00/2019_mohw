2a6c10a7605b2abc64451d86ebe0f25743841204
veelatseng <veela.tzeng@gmail.com>
2024-08-16 16:27:05 +0800

    修改login頁面

--- js/hyui.js
+++ js/hyui.js
@@ -625,6 +625,72 @@ $(function() {
     $('.related_tab .tabs>.tabItem:first-child>a').trigger('click');
     tabSetRelated();
 
+
+    /*-----------------------------------*/
+    ////////////////多組Tab4////////////////
+    /*-----------------------------------*/
+    var resizeTimer3;
+    _window.resize(function() {
+        clearTimeout(resizeTimer3);
+        resizeTimer3 = setTimeout(function() {
+            ww = _window.outerWidth();
+            tabSetLogin();
+        }, 50);
+    });
+
+    function tabSetLogin() {
+        $('.login_Tab').each(function() {
+            var _tab = $(this),
+            _tabItem = _tab.find('.tabItem'),
+            _tabItemA = _tabItem.children('a'),
+            _tabContent = _tab.find('.tabContent'),
+            tabwidth = _tab.width(),
+            tabItemHeight = _tabItem.outerHeight(),
+            tabContentHeight = _tab.find('.active').next().innerHeight(),
+            tiGap = 0,
+            tabItemLength = _tabItem.length,
+            tabItemWidth;
+            _tab.find('.active').next('.tabContent').show();
+            if (ww >= wwSmall) {
+                _tabContent.css('top', tabItemHeight);
+                _tab.height(tabContentHeight + tabItemHeight);
+                // tabItemWidth = (tabwidth - (tabItemLength - 1) * tiGap) / tabItemLength;
+                tabItemWidth =_tabItem.width();
+                // _tabItem.width(tabItemWidth).css('margin-left', tiGap);
+                _tabItem.first().css('margin-left', 0);
+                // _tabItem.last().css({ 'position': 'absolute', 'top': 0, 'right': 0 }).width(tabItemWidth);
+            } else {
+                _tab.css('height', 'auto');
+                // _tabItem.width(tabwidth);
+                _tabItem.css('margin-left', 0).last().css('position', 'relative');
+            }
+            _tabItemA.focus(tabs);
+            _tabItemA.click(tabs);
+
+            function tabs(e) {
+                var _tabItemNow = $(this).parent(),
+                tvp = _tab.offset().top,
+                tabIndex = _tabItemNow.index() / 2,
+                scollDistance = tvp + tabItemHeight * tabIndex - hh;
+                _tabItem.removeClass('active');
+                _tabItemNow.addClass('active');
+                if (ww <= wwSmall) {
+                    _tabItem.not('.active').next().slideUp();
+                    _tabItemNow.next().slideDown();
+                    $("html,body").stop(true, false).animate({ scrollTop: scollDistance });
+                } else {
+                    _tabItem.not('.active').next().hide();
+                    _tabItemNow.next().show();
+                    tabContentHeight = _tabItemNow.next().innerHeight();
+                    _tab.height(tabContentHeight + tabItemHeight);
+                }
+                e.preventDefault();
+            }
+        });
+    }
+    $('.login_Tab .tabs>.tabItem:first-child>a').trigger('click');
+    tabSetLogin();
+
     /*-----------------------------------*/
     ///////////////置頂go to top////////////
     /*-----------------------------------*/
