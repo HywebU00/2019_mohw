80f29d0d5969bf1d498048cc39d8fd434d1c6778
veelatseng <veela.tzeng@gmail.com>
2025-01-16 11:26:00 +0800

    修改Banner文字及新增密碼眼睛

--- js/customize.js
+++ js/customize.js
@@ -152,6 +152,29 @@ $(function() {
     e.preventDefault();
   });
 })
+
+ // password_toggle
+ var passShow = false;
+ $('.password_toggle').each(function (index, el) {
+   $(this)
+     .find('.btn-icon')
+     .off()
+     .click(function (e) {
+       if (!passShow) {
+         $(this).children('i').removeClass().addClass('i_show');
+         $(this).parents('.password_toggle').find('input[type="password"]').attr('type', 'text');
+         passShow = true;
+         // console.log(passShow);
+       } else {
+         $(this).children('i').removeClass().addClass('i_hide');
+         $(this).parents('.password_toggle').find('input[type="text"]').attr('type', 'password');
+         passShow = false;
+         // console.log(passShow);
+       }
+       e.preventDefault();
+     });
+ });
+
 $(function() {
   // chart1 近年護理人員統計
   $('.chart1 .chart').highcharts({
